fa7626a403ed7b4fad938b0db366d0d8afc635a8
Aadithya Vasudevan <68108339+Aadithya-19@users.noreply.github.com>
2021-01-29 16:00:25 +0530

    Add files via upload

---
+++ PythonProject.py
@@ -0,0 +1,31 @@
+import random
+
+r = random.randint(1,10)
+
+
+choices = 5
+
+
+while(choice>=0):
+    inp = (input("Enter Value : "))
+     choice = choice - 1
+
+#introduction = (input("Write About yourself... "))
+#print(introduction)
+
+#charcount = 0
+
+#wordcount = 1
+
+#for cw in introduction:
+ #   charcount = charcount+1
+
+
+
+  #  if(cw == " "):
+   #     wordcount = wordcount + 1
+
+
+#print(charcount)
+#print(wordcount)
+
